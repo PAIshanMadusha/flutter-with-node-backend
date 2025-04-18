6d0f07c5e4d80f54fd48eb8c946d4b16971f007f
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2025-04-18 15:56:03 +0530

    Implementing get all users, get Single User, Update and Delete User

--- node_backend/controllers/userController.js
+++ node_backend/controllers/userController.js
@@ -1,14 +1,76 @@
 const User = require("../models/userModel");
 
 //Create User
-exports.createUser = async(req, res) =>{
-    try{
+exports.createUser = async (req, res) => {
+    try {
         const user = new User(req.body);
         //Store the User
         await user.save();
 
         res.status(201).json(user);
-    }catch(error){
-        res.status(400).json({error:error.message});
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    };
+};
+
+//Get All Users
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        if (!users) {
+            return res.status(404).json({ message: "Users Not Found!" });
+        } else {
+            res.json(users);
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    };
+};
+
+//Get a Single User
+exports.getSingleUser = async (req, res) => {
+    try {
+        const userId = req.params.id.trim();
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User Not Found!" });
+        } else {
+            res.json(user);
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    };
+};
+
+//Update a User
+exports.updateUser = async (req, res) => {
+    try {
+        const userId = req.params.id.trim();
+        const user = await User.findByIdAndUpdate(userId, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (!user) {
+            return res.status(404).json({ message: "User Not Found!" })
+        } else {
+            res.json(user);
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    };
+};
+
+//Delete a User
+exports.deleteUser = async (req, res) => {
+    try {
+        const userId = req.params.id.trim();
+        const user = await User.findByIdAndDelete(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User Not Found!" })
+        } else {
+            res.json({ message: "User Deleted Successfully!" });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
 };
--- node_backend/routes/userRoute.js
+++ node_backend/routes/userRoute.js
@@ -2,5 +2,11 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.post("/users", userController.createUser);
+//✅Tested With PostMan
+router.post("/users", userController.createUser);//✅
+router.get("/users/:id", userController.getSingleUser);//✅
+router.get("/users", userController.getAllUsers);//✅
+router.put("/users/update/:id", userController.updateUser);//✅
+router.delete("/users/delete/:id", userController.deleteUser);//✅
+
 module.exports = router;
